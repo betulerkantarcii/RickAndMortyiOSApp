8d8c3168086e30fa80c9afa0b65d911dce39e830
Betül Erkantarcı <betule_98@hotmail.com>
2023-05-07 14:31:49 +0300

    Created Rick and Morty project

---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Betül Erkantarcı on 7.05.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Betül Erkantarcı on 7.05.2023.
+//
+
+import Foundation
