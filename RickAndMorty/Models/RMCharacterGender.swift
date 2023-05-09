a4961a89403ea4e5a03cd9603d3c8b084a5df15c
Betül Erkantarcı <betule_98@hotmail.com>
2023-05-09 12:02:40 +0300

    API request created

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Betül Erkantarcı on 7.05.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,8 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Betül Erkantarcı on 7.05.2023.
+//
+
+import Foundation
